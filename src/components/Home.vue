1c29a50e9543bc0b582eafead43ddd18646e0ae2
Pranjal  Shrivastav <pranjalshrivastav@Pranjals-MacBook-Pro.local>
2021-08-18 16:27:35 +0530

    Added components

---
+++ src/components/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="header">
+    Virtual Random Keyboard
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.header{
+  display: flex;
+  align-content: flex-end;
+  justify-content: flex-end;
+  margin-top: 25px;
+  right: 5px;
+  font-size: 22px;
+  background: white;
+  /* background: black; */
+  position: absolute;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+
+</style>
---
+++ src/components/Jumbotron.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+      <!-- <textarea name="area" id="area" cols="30" rows="10" value=""/> -->
+       <textarea
+      ref='focusMe'
+      type="text"
+      v-model="input"
+      class="jumbotron"/>
+  
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Jumbotron',
+    props:[],
+
+
+}
+</script>
+
+<style>
+.jumbotron{
+ 
+  margin-top: 120px;
+  height: 90px;
+  width: 90%;
+  font-size: 20px;
+}
+</style>
